9fd1ee8f54d2d28ac9ecbbd514ab68cf62743645
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2026-05-21 01:33:00 +0000

    Remove obsolete configs

--- apps/hmi/src/config.py
+++ apps/hmi/src/config.py
@@ -2,10 +2,7 @@ import os
 
 
 class Config:
-    APPLICATION_ROOT = os.environ.get("OPENFACTORY_ROOT_PATH", "")
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     FLASK_ADMIN_SWATCH = 'cerulean'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///hmi.db'
-    KSQL_HOST = os.environ.get('KSQLDB_URL') or 'http://localhost:8088'
-    KAFKA_BROKER = os.environ.get('KAFKA_BROKER') or 'localhost:9092'
     NC_FILES_FOLDER = None
